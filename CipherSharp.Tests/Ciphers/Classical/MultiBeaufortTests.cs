092358106f1f97a7f64056d074e2cf22df105136
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-17 21:41:23 +0000

    added multi beaufort cipher

---
+++ CipherSharp.Tests/Ciphers/Classical/MultiBeaufortTests.cs
@@ -0,0 +1,37 @@
+﻿using CipherSharp.Ciphers.Classical;
+using System;
+using Xunit;
+
+namespace CipherSharp.Tests.Ciphers.Classical
+{
+    public class MultiBeaufortTests
+    {
+        [Fact]
+        public void Encode_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string text = "helloworld";
+            string[] keys = new string[2] { "test", "key"};
+
+            // Act
+            var result = MultiBeaufort.Encode(text, keys);
+
+            // Assert
+            Assert.Equal("YERCZQGIWI", result);
+        }
+
+        [Fact]
+        public void Decode_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string text = "YERCZQGIWI";
+            string[] keys = new string[2] { "test", "key" };
+
+            // Act
+            var result = MultiBeaufort.Decode(text, keys);
+
+            // Assert
+            Assert.Equal("HELLOWORLD", result);
+        }
+    }
+}
