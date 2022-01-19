1a9006a75cdba38b39f41e81b216284095640ab4
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-19 02:12:06 +0000

    added hutton cipher

---
+++ CipherSharp.Tests/Ciphers/Substitution/HuttonTests.cs
@@ -0,0 +1,36 @@
+﻿using CipherSharp.Ciphers.Substitution;
+using Xunit;
+
+namespace CipherSharp.Tests.Ciphers.Substitution
+{
+    public class HuttonTests
+    {
+        [Fact]
+        public void Encode_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string text = "helloworld";
+            string[] keys = new string[2] { "test", "key" };
+
+            // Act
+            var result = Hutton.Encode(text, keys);
+
+            // Assert
+            Assert.Equal("NQPUTMXMKR", result);
+        }
+
+        [Fact]
+        public void Decode_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string text = "NQPUTMXMKR";
+            string[] keys = new string[2] { "test", "key" };
+
+            // Act
+            var result = Hutton.Decode(text, keys);
+
+            // Assert
+            Assert.Equal("HELLOWORLD", result);
+        }
+    }
+}
