f1de98740a28579760afa20c20accbedb92db284
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-19 02:59:56 +0000

    added quagmire one cipher

---
+++ CipherSharp.Tests/Ciphers/Polyalphabetic/QuagmireTests.cs
@@ -0,0 +1,34 @@
+﻿using CipherSharp.Ciphers.Polyalphabetic;
+using Xunit;
+
+namespace CipherSharp.Tests.Ciphers.Polyalphabetic
+{
+    public class QuagmireTests
+    {
+        [Fact]
+        public void Encode_BasicParameters_ReturnsCipherText()
+        {
+            // Arrange
+            string text = "helloworld";
+            string[] keys = new string[2] { "test", "key" };
+            // Act
+            var result = Quagmire.One.Encode(text, keys);
+
+            // Assert
+            Assert.Equal("QCIURRXUIN", result);
+        }
+
+        [Fact]
+        public void Decode_BasicParameters_ReturnsPlainText()
+        {
+            // Arrange
+            string text = "QCIURRXUIN";
+            string[] keys = new string[2] { "test", "key" };
+            // Act
+            var result = Quagmire.One.Decode(text, keys);
+
+            // Assert
+            Assert.Equal("HELLOWORLD", result);
+        }
+    }
+}
